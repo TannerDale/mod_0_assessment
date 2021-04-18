f80bca2690203c2fd3cfc5ddda78aff7a2784d02
Tanner Dale <tanner@tannerdale.com>
2021-04-17 20:11:53 -0500

    Add school model

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+school = {
+  name: "North Oaks",
+  location: "Austin, TX",
+  teachers: 56,
+  students: 278,
+  elementary_school: true,
+  bad_school: false,
+  grades: [1, 2, 3, 4, 5],
+  subjects: ["math", "science", "history", "english"]
+}
