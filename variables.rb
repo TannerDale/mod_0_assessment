6f4eeff7a79ac530949c28f927063e1ad3f52a99
Tanner Dale <tanner@tannerdale.com>
2021-04-17 20:08:17 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,15 @@
+famous_person = "Albert Einstien"
+is_active = false
+
+famous_person = "Bruce Willis"
+
+num_3 = 67
+num_9 = 42
+
+sum = num_3 + num_9
+
+animals = ["snake", "lizard", "lion"]
+
+animals.shift
+
+puts famous_person.length
