1214efb31a4cf804bcc575ba99bdb0ea70f96911
Tanner Dale <tanner@tannerdale.com>
2021-04-17 20:14:26 -0500

    Add pet model

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+pet = {
+  name: "Rifle",
+  animal: "dog",
+  age: "12",
+  weight: "15",  # lbs
+  good_dog: true,
+  alive: false,
+  colors: ["black", "brown", "grey"],
+  favorite_toys: ["socks", "old cans", "sqeaky chickens"]
+}
