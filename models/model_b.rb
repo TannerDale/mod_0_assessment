c5b54c61e41d6b0d37a7d63092509e2b2943b7cf
Tanner Dale <tanner@tannerdale.com>
2021-04-24 16:20:04 -0500

    Change weight, age variables to ints

--- models/model_b.rb
+++ models/model_b.rb
@@ -1,8 +1,8 @@
 pet = {
   name: "Rifle",
   animal: "dog",
-  age: "12",
-  weight: "15",  # lbs
+  age: 12,
+  weight: 15,  # lbs
   good_dog: true,
   alive: false,
   colors: ["black", "brown", "grey"],
